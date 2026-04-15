30e411d1e17ebb8e5b8b636f777a0bb0ba379cf1
hs6uej <hs6uej@gmail.com>
2026-04-16 01:32:40 +0700

    add module

--- customer-portal/next.config.mjs
+++ customer-portal/next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+
 const nextConfig = {
   output: 'standalone',
   // Ensure that env variables used at build time are passed
   // Any public env variables should be prefixed with NEXT_PUBLIC_
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
